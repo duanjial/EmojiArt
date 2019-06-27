908e3a59f391e73c2c2454dd9b7c9e80a7b759c0
Jiale Duan <carlo_duan@hotmail.com>
2019-06-27 16:11:23 -0400

    Added missing files

---
+++ EmojiArt/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Jiale Duan on 2019-06-26.
+//  Copyright © 2019 Jiale Duan. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+}
